e865b3aeba53b792c3bbb9fd882d9fdc87a6191f
anushya-2024-aia <anushyae40@gmail.com>
2024-10-27 20:27:04 +0530

    Add files via upload

---
+++ protfolio-streamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("welcome to protfolio")
+st.subheader("Hello,I am Anushya")
+st.write("I am a student of B.Tech in AIDS")
+
+#introduction Section
+st.header("Introduction")
+st.write("I am a student of kgisl Institute of Technology and Iam currently in my first year of my bachelors's degree in Artificial Inteligence and data science.")
+st.write("I chosen this course because I am interested in learning about the basics of AI and how it works")
+st.write("My vision in these 4 years is to become a data scientist and to work in a reputed company with good salary")
+#skills section
+st.header("My Skills")
+st.write("As I am a first year student i am not familiar with any programming language.")
+st.write("I am a beginner in python and i am learning about it")
+st.write("Apart from this i know basics of python.")
+#email section
+st.header("Email")
+st.write("You can reach me at:anushyae55@gmail.com")
+#contact section
+st.header("Contact")
+st.write("You can contact me at:+918012111372")
